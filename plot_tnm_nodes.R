3aab82987df75d5db294914e27affd10576b7d59
Charles <qiuxiz@ics.uci.edu>
2018-06-11 19:25:00 -0700

    Using a better aggregator to reorg data frame

--- plot_tnm_nodes.R
+++ plot_tnm_nodes.R
@@ -6,38 +6,34 @@ library(reshape2)
 
 cat("Processing...", "\n")
 sr_interval <- simu_res[simu_res$metric == "interval_mean", ]
-sr_int_mean <- aggregate.data.frame(
+sr_int_proc <- aggregate.data.frame(
     x = list(
         value       = sr_interval$value
     ), by = list(
         selector    = sr_interval$selector,
         num_nodes   = sr_interval$num_nodes
-    ), FUN = mean
-)
-sr_int_sd <- aggregate.data.frame(
-    x = list(
-        value       = sr_interval$value
-    ), by = list(
-        selector    = sr_interval$selector,
-        num_nodes   = sr_interval$num_nodes
-    ), FUN = sd
+    ), FUN = function(x) {
+        c(
+            mean    = mean(x),
+            sd      = sd(x),
+            se      = sd(x) * qnorm(0.975) / sqrt(length(x))
+        )
+    }
 )
 sr_cali_t <- simu_res[simu_res$metric == "cali_t_mean", ]
-sr_cal_mean <- aggregate.data.frame(
+sr_cal_proc <- aggregate.data.frame(
     x = list(
         value       = sr_cali_t$value
     ), by = list(
         selector    = sr_cali_t$selector,
         num_nodes   = sr_cali_t$num_nodes
-    ), FUN = mean
-)
-sr_cal_sd <- aggregate.data.frame(
-    x = list(
-        value       = sr_cali_t$value
-    ), by = list(
-        selector    = sr_cali_t$selector,
-        num_nodes   = sr_cali_t$num_nodes
-    ), FUN = sd
+    ), FUN = function(x) {
+        c(
+            mean    = mean(x),
+            sd      = sd(x),
+            se      = sd(x) * qnorm(0.975) / sqrt(length(x))
+        )
+    }
 )
 
 # cat("Rendering...", "\n")
