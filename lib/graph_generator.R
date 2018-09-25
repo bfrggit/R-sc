63ba63d8ab86076ca01b18d7021aa33439257266
Charles Zhu <qiuxiz@ics.uci.edu>
2018-09-25 11:10:52 -0700

    Added graph gen and igraph-based R prep script

---
+++ lib/graph_generator.R
@@ -0,0 +1,69 @@
+# graph_generator.R
+#
+# Created: 2018-09-24
+#  Author: Charles Zhu
+#
+if(!exists("EX_GRAPH_GENERATOR_R")) {
+    EX_GRAPH_GENERATOR_R <<- TRUE
+
+source("lib/basic.R")
+
+generate_connected_digraph_adj_matrix_random_loop <- function(
+    num_spots,
+    num_edges,
+    rand_gen_f    # func that randomly generates a positive int when called
+) {
+    stopifnot(is.integer(num_spots))
+    stopifnot(num_spots > 1L)
+
+    stopifnot(is.integer(num_edges))
+    stopifnot(num_edges >= num_spots)
+    stopifnot(num_edges <= num_spots * num_spots - num_spots)
+
+    stopifnot(is.function(rand_gen_f))
+
+    spot_indexes <- 1L:num_spots
+    adj_mat <- matrix(0L, nrow = num_spots, ncol = num_spots)
+    colnames(adj_mat) <- rownames(adj_mat) <- z_nd_str("spot", num_spots)
+    self_edge_indexes <- spot_indexes * (num_spots + 1L) - num_spots
+
+    # generate a random directed loop that contains all spots
+    # this is the minimum connected digraph on these spots
+    # this is done as a random permutation
+    perm = sample(spot_indexes, size = num_spots)
+    for(jnd in 1L:(num_spots - 1L)) {
+        adj_mat[perm[jnd], perm[jnd + 1L]] = rand_gen_f()
+    }
+    adj_mat[perm[num_spots], perm[1L]] = rand_gen_f() # close the loop
+
+    # paranoid check
+    # stopifnot(is.integer(adj_mat))
+    # stopifnot(all(adj_mat[self_edge_indexes] == 0L))
+    # stopifnot(all(adj_mat >= 0L))
+    # stopifnot(sum(adj_mat > 0L) == num_spots)
+
+    # add extra edges to fulfill the total num of edges
+    if(num_edges > num_spots) {
+        adj_mat[
+            sample(
+                setdiff(
+                    which(adj_mat <= 0L),
+                    self_edge_indexes # avoid spot-to-self edges
+                ),
+                size = num_edges - num_spots
+            )
+        ] <- rand_gen_f()
+    }
+
+    # paranoid check
+    stopifnot(is.integer(adj_mat))
+    stopifnot(all(adj_mat[self_edge_indexes] == 0L))
+    stopifnot(all(adj_mat >= 0L))
+    stopifnot(sum(adj_mat > 0L) == num_edges)
+
+    adj_mat # RETURN
+}
+
+library(igraph)
+
+} # ENDIF
---
+++ prep/prep_graph_random_loop_unif.R
@@ -0,0 +1,92 @@
+#!/usr/bin/env Rscript
+#
+# prep_graph_random_loop_unif.R
+#
+# Created: 2018-09-24
+#  Author: Charles Zhu
+
+suppressPackageStartupMessages(require(optparse))
+
+opt_list = list(
+    make_option(
+        c("-O", "--output_file"),
+        action = "store", default = NA, type = "character",
+        help = "Output filename"),
+    make_option(
+        c("-s", "--random_seed"),
+        action = "store", default = NA, type = "integer",
+        help = "Random seed, default = %default"),
+    make_option(
+        c("-L", "--num_spots"),
+        action = "store", default = NA, type = "integer",
+        help = "Number of spots"),
+    make_option(
+        c("-e", "--num_edges"),
+        action = "store", default = NA, type = "integer",
+        help = "Number of edges"),
+    make_option(
+        c("--weight_lower"),
+        action = "store", default = 30, type = "integer",
+        help = "Minimum edge weight (movement time), default = %default"),
+    make_option(
+        c("--weight_upper"),
+        action = "store", default = 300, type = "integer",
+        help = "Maximum edge weight (movement time), default = %default")
+)
+opt_obj = OptionParser(option_list = opt_list)
+opt = parse_args(opt_obj)
+
+if(is.na(opt$output_file)) {
+    print_help(object = opt_obj)
+    stop("Must specify output filename.")
+}
+if(is.na(opt$num_spots) || is.na(opt$num_edges)) {
+    print_help(object = opt_obj)
+    stop("Must specify number of spots and number of edges.")
+}
+
+stopifnot(!file.exists(opt$output_file))
+stopifnot(file.create(opt$output_file))
+stopifnot(opt$num_spots > 1L)
+stopifnot(opt$num_edges >= opt$num_spots)
+stopifnot(opt$num_edges <= opt$num_spots * opt$num_spots - opt$num_spots)
+
+NUM_SPOTS <<- opt$num_spots
+NUM_EDGES <<- opt$num_edges
+lockBinding("NUM_SPOTS", globalenv())
+lockBinding("NUM_EDGES", globalenv())
+
+if(!is.na(opt$random_seed)) {
+    set.seed(opt$random_seed)
+}
+
+suppressPackageStartupMessages(source("lib/graph_generator.R"))
+
+WEIGHT_GEN_F <<- function() {
+    as.integer(
+        sample(
+            opt$weight_lower:opt$weight_upper,
+            size = 1L,
+            replace = TRUE
+        )
+    ) # RETURN
+}
+lockBinding("WEIGHT_GEN_F", globalenv())
+
+map_graph_adj_matrix <- generate_connected_digraph_adj_matrix_random_loop(
+    num_spots   = NUM_SPOTS,
+    num_edges   = opt$num_edges,
+    rand_gen_f  = WEIGHT_GEN_F
+)
+map_graph <- graph_from_adjacency_matrix(
+    adjmatrix   = map_graph_adj_matrix,
+    mode        = "directed",
+    weighted    = TRUE,
+    diag        = FALSE
+)
+
+save(
+    NUM_SPOTS, NUM_EDGES,
+    map_graph_adj_matrix, map_graph,
+    file = opt$output_file
+)
