e9ee7691d4b004151aeb051dd67f9595548c2165
Charles Zhu <qiuxiz@ics.uci.edu>
2018-11-25 20:16:38 -0800

    Initial commit for GA-based mTSP

---
+++ solution/pp_ga_1.R
@@ -0,0 +1,25 @@
+# pp_ga_1.R
+#
+# Created: 2018-11-20
+#  Author: Charles Zhu
+#
+# mTSP (path planning) solver
+# GA solution, single chromosome
+#
+# References:
+#   Mohammad Sedighpour, Majid Yousefikhoshbakht, and Narges Mahmoodi Darani.
+#       "An effective genetic algorithm for solving
+#           the multiple traveling salesman problem."
+#       Journal of Optimization in Industrial Engineering 8 (2012): 73-79.
+#       http://www.qjie.ir/article_89_0.html
+
+if(!exists("EX_PP_GA_1_R")) {
+    EX_PP_GA_1_R <<- TRUE
+
+get_multi_paths_ga_1 <<- function(
+
+) {
+
+}
+
+} # ENDIF
